e2f5106d5dbbcb21a6fb1e6ec3a9829fb5340c2b
khanasfahankhan-sys <khanasfahan.khan@gmail.com>
2026-04-01 02:44:36 +0530

    add CORS headers for React connection

--- backend/pingoo_backend/settings.py
+++ backend/pingoo_backend/settings.py
@@ -45,11 +45,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'pingoo_backend.urls'
